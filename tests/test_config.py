198f7427bd9a9cb8d1401f81c6b47b635c4d62a3
Matt Kramer <mkramer@anaconda.com>
2022-09-03 23:43:04 -0500

    Split test

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,11 +5,11 @@ from textwrap import dedent
 
 import pytest
 
-import rusty_mooring
+from rusty_mooring import Config
 
 
 @pytest.fixture()
-def with_toml_file() -> None:
+def config() -> Config:
     """Write a TOML file into the current directory."""
     contents = dedent(
         """\
@@ -30,20 +30,25 @@ def with_toml_file() -> None:
         axial_stiffness = 9.83e8
         """
     )
-    with Path("test.toml").open("w") as fp:
+    filename = "test.toml"
+    with Path(filename).open("w") as fp:
         fp.write(contents)
 
+    config = Config.from_file(filename)
+    return config
 
-@pytest.mark.usefixtures("with_toml_file")
-def test_config_from_file() -> None:
+
+def test_general_config_from_file(config: Config) -> None:
     """Load a Config from a TOML file."""
-    config = rusty_mooring.Config.from_file("test.toml")
     general = config.general
     assert general.units == "metric"
     assert general.gravity == 9.81
     assert general.water_density == 1025.9
     assert not hasattr(general, "extra_value")
 
+
+def test_line_type_config_from_file(config: Config) -> None:
+    """Load line types from TOML file."""
     polyester = config.line_type["polyester"]
     assert polyester.diameter == 0.233
     assert polyester.mass_per_length == 53.7
@@ -58,5 +63,5 @@ def test_config_from_file() -> None:
 def test_config_from_file_missing_raises_error() -> None:
     """An exception is raised if the file doesn't exist."""
     with pytest.raises(FileNotFoundError) as exc_info:
-        rusty_mooring.Config.from_file("test.toml")
+        Config.from_file("test.toml")
     assert str(exc_info.value) == "File 'test.toml' not found"
